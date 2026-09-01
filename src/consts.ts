7f438e16339bf1c163742aafeeab7079fe32ccf4
JT Williams <williams.jt891419@protonmail.com>
2026-09-01 05:02:42 -0400

    Establish Fortress foundation and apply Punk Positronics branding

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Punk Positronics";
+export const SITE_DESCRIPTION = "Removing the parasitic middlemen from digital and physical infrastructure to empower community sovereignty.";
